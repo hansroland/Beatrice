93d309fc37ab35bad0fc0c1125e30c038a48a8ee
roland <rsx@bluewin.ch>
2014-05-09 14:48:26 +0200

    First Versions

---
+++ Rename.hs
@@ -0,0 +1,69 @@
+-- -------------------------------------------------------------------
+-- Skript zum Umbenennen von Fotos
+-- -------------------------------------------------------------------
+--
+-- Umbennenen von Pilzfotos für Beatrice
+--
+-- Input: Verzeichnis mit Filenamen der Art:
+--        Agaricus campestris-Feld-Champignon-5.JPG
+--
+-- Output: Diese Filenamen werden geändert auf
+--         JG0001 Agaricus campestris.jpg
+--           wobei hinter JG (für Juerg Gilgen eine fortlaufende 
+--           Nr folgt.
+--
+-- Je nach Funktion in processPair werden die Fotos kopiert oder
+--         umbenannt.
+--
+-- Das Verzeichnis ist hart in der Funktion fromDir hinterlegt
+--
+-- -------------------------------------------------------------------
+import System.Directory(getDirectoryContents, copyFile, renameFile)
+import Data.List(isSuffixOf, sort)
+import Data.Char (toUpper)
+
+main :: IO()
+main = copyAndRenameFiles fromDir
+
+-- Directory, in dem die .jpg Files gelesen werden
+fromDir :: FilePath
+-- fromDir = "/home/roland/Beatrice/"
+fromDir = "/home/roland/Beatrice/JGilgen_2/"
+
+-- Directory, in das die Kopien geschrieben werden
+toDir :: FilePath
+toDir = fromDir ++ "Renames/"
+
+-- Hauptverarbeitung:
+-- Alle Filenamen im Direcotry werden gelesen
+-- Es werden die .jpg Files herausgefiltert
+-- Die Filenamen werden sortiert
+-- Es wird eine Liste von Paaren mit den alten und neuen Filenamen erstellt
+-- Die Datien werden umbenennt / kopiert
+copyAndRenameFiles :: FilePath -> IO()
+copyAndRenameFiles path = do
+  files <- getDirectoryContents path
+  mapM_ processPair $ (newNames . sort . selectJpgs ) files
+    where 
+      selectJpgs = filter $ isSuffixOf ".JPG". map toUpper
+
+-- Creates from a list of filenames a list of pairs with
+-- old filename and new filename.
+newNames :: [String] -> [(String, String)]
+newNames files = lnewNames 2000 files
+    where
+      lnewNames n []       = []
+      lnewNames n (f : fs) = (f, newName n f) : lnewNames (n + 1) fs
+
+-- create a new name from an integer and an existing filename
+newName :: Int -> String -> String
+newName n fn = "JG" ++ strNum n ++ " " ++ takeWhile (/= '-') fn ++ ".jpg"
+   where 
+     strNum :: Int -> String 
+     strNum n = tail $ show $ 10000 + n
+
+-- Ein Paar bestehend aus einem alten und neuen Filnamen wird verarbeitet.
+-- Kopieren oder Umbenennen
+processPair :: (String, String) -> IO()
+-- processPair (old, new) = renameFile (fromDir ++ old) (fromDir ++ new)
+processPair (old, new) = copyFile (fromDir ++ old) (toDir ++ new)
---
+++ createIndex.hs
@@ -0,0 +1,74 @@
+-- -------------------------------------------------------------------
+-- Skript zum Erstellen eines CSV Files mit Filenamen; Pilznamen
+-- -------------------------------------------------------------------
+--
+-- Umbennenen von Pilzfotos für Beatrice
+--
+-- Input: Verzeichnis mit Filenamen der Art:
+--        JG0001 Agaricus campestris.jpg
+--        JG0223 Cortinarius (Phl.) cyanobasalis.jpg
+--
+-- Output: CSV Datei mit Filenamen und lateinischem Namen 
+--
+-- Zuerst muss mit dem Script Rename.hs das Umbenennen durchgeführt werden.
+--
+--
+-- Das Verzeichnis ist hart in der Funktion fromDir hinterlegt
+--
+-- -------------------------------------------------------------------
+import System.Directory(getDirectoryContents, copyFile, renameFile)
+import System.IO
+import Data.List(isSuffixOf, sort)
+import Data.Char (toUpper)
+
+main :: IO()
+main = createIndex fromDir
+
+-- Directory, in dem die Files gelesen werden
+fromDir :: FilePath
+-- fromDir = "/home/roland/Beatrice/"
+fromDir = "/home/roland/Beatrice/JGilgen_2/Renames/"
+
+
+-- Hauptverarbeitung:
+-- Alle Filenamen im Direcotry werden gelesen
+-- Es werden die .jpg Files herausgefiltert
+-- Der lateinische namen wird extrahiert
+createIndex :: FilePath -> IO()
+createIndex path = do
+  hFile <- openFile (fromDir ++ "index.csv") WriteMode
+  files <- getDirectoryContents path
+  lines <- return $ map getLatName $ sort $ selectJpgs files 
+  mapM_ (hPutStrLn hFile) lines
+    where 
+      selectJpgs = filter $ isSuffixOf ".JPG". map toUpper
+
+-- Entfernt das .jpg am Ende der Datei
+removeTrailingJPG :: String -> String
+removeTrailingJPG  = reverse . (drop 4 ) . reverse
+
+-- Entfernt die LaufNr am Anfang
+removeHeader :: String -> String
+removeHeader = drop 7
+
+removeMultBlanks :: String -> String
+removeMultBlanks [] = []
+removeMultBlanks (' ' : []) = []
+removeMultBlanks (' ' : ' ' : xs) = removeMultBlanks (' ' :xs)
+removeMultBlanks (x : xs) = x: removeMultBlanks xs
+
+-- Entfernt den Text in Klammern
+removeParens :: String -> String
+-- removeParens = (takeWhile /= lParen)
+-- removeParens =  (dropWhile (/= ')')) . (takeWhile (/= '('))
+removeParens fn =  first ++ last
+  where 
+     first = takeWhile (/= '(') fn
+     last = if last' == [] then [] else tail last'
+     last' = dropWhile (/= ')') fn
+    
+createCSVLine :: String -> String 
+createCSVLine = removeMultBlanks . removeParens . removeTrailingJPG . removeHeader
+
+getLatName :: String -> String
+getLatName  fn =  fn ++ ";" ++ createCSVLine fn ++ ";"
